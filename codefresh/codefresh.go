3eb9d0965673dfd56b67812c4fb51e0bb3451886
daniel-codefresh <daniel.soifer@codefresh.io>
2021-08-17 09:41:52 +0300

    wip

--- codefresh/codefresh.go
+++ codefresh/codefresh.go
@@ -69,7 +69,7 @@ func getCodefreshBaseURL(ctx context.Context, kubeClient kubernetes.Interface, n
 
 func ReportEventToCodefresh(eventJson []byte, config *Config) error {
 	contentType := "application/json"
-	url := config.BaseURL + "/argo/api/events/event-payload"
+	url := config.BaseURL + "/2.0/api/events/event-payload"
 	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(eventJson))
 	req.Header.Set("Content-Type", contentType)
 	req.Header.Set("Authorization", config.AuthToken)
